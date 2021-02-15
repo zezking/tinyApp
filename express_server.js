16cdfdee5a0f7afbca0516d72a4b855cc7f6a680
zezking <zhaoenze001@gmail.com>
2021-02-15 21:26:56 +0000

    header ejs show ejs

--- express_server.js
+++ express_server.js
@@ -8,15 +8,17 @@ const urlDatabase = {
 };
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
+  };
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
