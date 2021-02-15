9ff7a9adda4a5c196b4a1b70b69ee32c81937728
zezking <zhaoenze001@gmail.com>
2021-02-15 20:28:32 +0000

    esj engine

--- express_server.js
+++ express_server.js
@@ -7,18 +7,16 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-app.get("/hello", (req, res) => {
-  res.send("<html><body><h1>Welcome to tinyApp</h1></body></html>\n");
-});
-
-app.get("/error", (req, res) => {
-  res.send("<html><body><h1>Wrong Username or Password</h1></body></html>\n");
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
 });
 
 app.listen(PORT, () => {
