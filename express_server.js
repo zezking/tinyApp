965fa7137176f59e52608636fc35fdb819805414
zezking <zhaoenze001@gmail.com>
2021-02-15 18:34:50 +0000

    create a hello page that create html content

--- express_server.js
+++ express_server.js
@@ -6,15 +6,15 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
-
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+app.get("/hello", (req, res) => {
+  res.send("<html><body><h1>Welcome to tinyApp</h1></body></html>\n");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
